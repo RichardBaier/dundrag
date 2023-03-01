8c31c7b2993bb16056ac5865b223754c4b3d8283
Payton <rekrapnotyap@gmail.com>
2023-02-28 19:04:19 -0700

    updated the file structure

---
+++ server/models/User.js
@@ -0,0 +1,5 @@
+const { Schema, model } = require('mongoose');
+
+
+
+module.exports = User;
---
+++ server/models/index.js
@@ -0,0 +1,8 @@
+const Bio = require('./Bio');
+const User = require('./User');
+const Character = require('./Character');
+const Skills = require('./Skills');
+const Spells = require('./Spells');
+
+
+module.exports = { Bio, User, Character, Skills, Spells };
