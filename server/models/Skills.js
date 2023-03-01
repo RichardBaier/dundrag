1d51de9dd215aa3124967ee35dd3e8f6e74ee0cc
Pipoxy <jparke5@hotmail.com>
2023-02-28 20:26:28 -0700

    added Skill model

--- server/models/Skills.js
+++ server/models/Skills.js
@@ -1,5 +1,16 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
+const skillSchema = new Schema({
+  skill_name: {
+    type: string,
+    required: true,
+  },
+  skill_description: {
+    type: string,
+    required: true,
+  },
+});
 
+const Skill = model("Skill", skillSchema);
 
-module.exports = Skills;
+module.exports = Skill;
