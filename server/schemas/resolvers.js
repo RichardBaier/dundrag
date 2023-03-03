913622ecae579a3d16f6d6fa5eb0db3a44fd62ea
Pipoxy <jparke5@hotmail.com>
2023-03-02 21:36:15 -0700

    added typedefs and resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,19 @@
-const { AuthenticationError } = require('apollo-server-express');
-const { User } = require('../models');
-const { signToken } = require('../utils/auth');
+const { AuthenticationError } = require("apollo-server-express");
+const { Profile, Character } = require("../models");
+const { signToken } = require("../utils/auth");
+
+const resolvers = {
+  Query: {
+    profiles: async () => {
+      return Profile.find().populate("characters");
+    },
+    profile: async (parent, { username }) => {
+      return Profile.findOne({ username }).populate("characters");
+    },
+    character: async (parent, { character_id }) => {
+      return Character.findOne({ _id: character_id });
+    },
+  },
+};
 
 module.exports = resolvers;
