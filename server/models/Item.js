9130e19c082ad3cc4485effe1a8af78d4c3af77f
Pipoxy <jparke5@hotmail.com>
2023-03-05 20:48:13 -0700

    added more to typedefs

--- server/models/Item.js
+++ server/models/Item.js
@@ -1,9 +1,10 @@
-const { Schema } = require('mongoose');
+const { Schema } = require("mongoose");
+
+const weaponSchema = new Schema({});
 
 const itemSchema = new Schema({
-  item_name: {type: String, required: true }, 
-  item_description: {type: String, required: true }, 
+  item_name: { type: String, required: true },
+  item_description: { type: String, required: true },
 });
 
 module.exports = itemSchema;
-
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -40,12 +40,43 @@ const typeDefs = gql`
     _id: ID!
     skill_name: String!
     skill_description: String!
+    ability_name: [abilitySchema]
+    ability_modifier: [abilitySchema]
   }
 
   type Spell {
     _id: ID!
     spell_name: String!
     spell_description: String!
+  }
+  type Pack {
+    name: String!
+    desc: [String!]
+    cost: Cost!
+    contents: [PackQuantity!]!
+  }
+
+  type Ammunition {
+    name: String!
+    desc: [String!]
+    quantity: Int!
+    cost: Cost!
+  }
+
+  type Armor {
+    name: String!
+    desc: [String!]
+    cost: Cost!
+    weight: Float
+    armor_class: ArmorClass!
+  }
+
+  type Weapon {
+    name: String!
+    cost: Cost!
+    desc: [String!]
+    damage: Damage
+    weapon_range: WeaponRange!
   }
 
   type Query {
@@ -54,7 +85,6 @@ const typeDefs = gql`
     characters: [Character]
     character_by_creator(creator: String!): [Character]
     character(character_id: ID!): Character
-
   }
 `;
 
