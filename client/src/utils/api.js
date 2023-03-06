c6f0c1cca021c1c5eeefbac24c5b0404413fa3ea
Spencer Merrill <canadianfaller7@gmail.com>
2023-03-06 03:19:55 -0700

    got login and signup to show up on home page and started working on utils folder

---
+++ client/src/utils/api.js
@@ -0,0 +1,32 @@
+// export const getMe = (token) => {
+//   return fetch('/api/users/me', {
+//     headers: {
+//       'Content-Type': 'application/json',
+//       authorization: `Bearer ${token}`,
+//     },
+//   });
+// };
+
+export const createProfile = (userData) => {
+  return fetch('/api/users', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(userData),
+  });
+};
+
+export const loginUser = (userData) => {
+  return fetch('/api/profile/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(userData),
+  });
+};
+
+export const dndFiveEApi = (query) => {
+  return fetch(`"https://www.dnd5eapi.co/api/${query}`);
+};
