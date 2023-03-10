a9f621240ed4babf2ff8b03f370db5d36746ad98
JHoang <JHoang6900@gmail.com>
2023-03-09 18:55:48 -0700

    please work for the presentation

--- client/src/pages/createChar/CreateChar.jsx
+++ client/src/pages/createChar/CreateChar.jsx
@@ -2,11 +2,6 @@ import React from 'react'
 import { Navbar } from '../../components'
 import "./createChar.css"
 
-<<<<<<< HEAD
-
-
-
-=======
 const dnd5eapiLink = "https://www.dnd5eapi.co/graphql";
 
 function apiCallClass() {
@@ -43,28 +38,14 @@ apiCallClass();
 //   }
 //   console.log(characterClasses);
 // });
->>>>>>> 1d2f50730578be769353649716f2b0500ea9fbd1
 
 const CreateChar = () => {
-
-
-
   return (
     <div>
       <Navbar />
       <h1>Create Your Character</h1>
-
     </div>
   )
 }
 
-
-
-
-
 export default CreateChar
-
-
-
-
-
