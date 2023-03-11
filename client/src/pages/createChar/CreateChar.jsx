0d3136f8138a0b669c9fa815741d7a48ca134234
JHoang <JHoang6900@gmail.com>
2023-03-10 23:55:08 -0700

    returned to the axios method

--- client/src/pages/createChar/CreateChar.jsx
+++ client/src/pages/createChar/CreateChar.jsx
@@ -1,14 +1,55 @@
-import React from 'react'
-import { Navbar } from '../../components'
-import "./createChar.css"
+import { Navbar } from "../../components";
+import "./createChar.css";
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
-const CreateChar = () => {
+const dnd5eapiLink = "https://www.dnd5eapi.co/graphql";
+
+function CreateChar() {
+  const [characterClass, setCharacterClass] = useState(null);
+
+  useEffect(() => {
+    axios.post(dnd5eapiLink, {
+      query: `
+      query Query {
+        classes {
+          name
+          class_levels {
+            level
+          }
+        }
+      }
+      `
+    }).then(result => {
+      setCharacterClass(result.data.data.classes[0]);
+      console.log(result)
+    }).catch(error => {
+      console.error(error);
+    });
+  }, []);
+
+  if (!characterClass) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <div>
-      <Navbar />
-      <h1>Create Your Character</h1>
+      <h2>{characterClass.name}</h2>
+      <p>characterClassName: {characterClass.class_levels[0].level}</p>
     </div>
-  )
+  );
 }
 
-export default CreateChar
+// data.data.classes[0].class_levels[0].level
+
+
+export default CreateChar;
+
+// {/* <label for="characterClassesList">Choose a class:</label>
+
+// <select name="classes" id="classes">
+//   <option value="volvo">Volvo</option>
+//   <option value="saab">Saab</option>
+//   <option value="mercedes">Mercedes</option>
+//   <option value="audi">Audi</option>
+// </select> */}
